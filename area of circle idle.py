c0e6ae9b64a06207501033d954bfaff7ee0db723
Srinivasrao2312 <112844809+Srinivasrao2312@users.noreply.github.com>
2022-09-09 08:38:48 +0530

    Update area of circle idle.py

--- area of circle idle.py
+++ area of circle idle.py
@@ -1,4 +1,4 @@
-import math as M  
-Radius = float (input ("Please enter the radius of the given circle: "))  
-area_of_the_circle = M.pi* Radius * Radius  
-print (" The area of the given circle is: ", area_of_the_circle)  
+a=3.14
+r=float(input("Enter the radius of circle:"))
+b=a*r*r
+print("area of circle",b)
